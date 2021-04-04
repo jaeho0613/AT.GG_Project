0c726a1124a9f30d2f02ca35e08efd6dc0c5d4ea
Jaeho Jeong <ywm2004@gmail.com>
2021-04-04 14:27:50 +0900

    #### [ BootStrap 설정 ] #### - main jsp 설정 - bootstrap cdn 설정 - jquery cdn 설정

--- AT.GG/src/main/java/com/jaeho/atgg/controller/MainController.java
+++ AT.GG/src/main/java/com/jaeho/atgg/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.jaeho.atgg;
+package com.jaeho.atgg.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
@@ -11,20 +11,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
-public class HomeController {
+public class MainController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String main(Locale locale, Model model) {
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -33,7 +24,6 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "main";
 	}
-	
 }
